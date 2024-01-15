2586c08475ffc0e2bb302739321b8bd214c2963c
RockyPraxe <filipio.svanda@gmail.com>
2024-01-15 18:39:30 +0000

    Added checkout views and templates

--- grip_shop/settings.py
+++ grip_shop/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'grip_shop.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -76,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
