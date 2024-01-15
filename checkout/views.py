fe76db776ffe42b593c003f881503faf83f654bd
RockyPraxe <filipio.svanda@gmail.com>
2024-01-15 19:37:42 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OEY0oKS855sDboyCCWnkfnpd6IWE2WVO4g7n4IsVCe6XfmEGnW8jLmbpFki9y88crBxFoo4lXCNJTWX9C9KxHxl00QbtHeZBP',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
