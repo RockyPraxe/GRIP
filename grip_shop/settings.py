af1060e96a8a77c2fa56c7344e52f29fbb3e7663
RockyPraxe <filipio.svanda@gmail.com>
2024-01-30 23:51:22 +0000

    debug=false

--- grip_shop/settings.py
+++ grip_shop/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+
+DEBUG = 'DEVELOPMENT' in os.environ
 
 
 ALLOWED_HOSTS = ['8000-rockypraxe-grip-tha0h5ot373.ws-eu107.gitpod.io', 'grip-e-shop-25bf5d916437.herokuapp.com', 'localhost']
