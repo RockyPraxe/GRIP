6db97d604de77f2891295794a35e78efb199fd0a
RockyPraxe <filipio.svanda@gmail.com>
2024-04-12 12:55:30 +0000

    Added contact_form to the admin and fix 3rd model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import contact_form
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+
+@admin.register(contact_form)
+class ContactAdmin(SummernoteModelAdmin):
+
+    list_display = ('reason', 'email', 'subject', 'submission_date', 'message')
